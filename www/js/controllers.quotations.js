f4bf35c46668ee733f87aae00efe6d965fdf043c
JerryHu <jerry_hu@wistron.com>
2016-11-06 21:07:40 +0800

    修改報價單

--- www/js/controllers.quotations.js
+++ www/js/controllers.quotations.js
@@ -5,6 +5,7 @@ angular.module('starter')
     var ctrl = $scope;
     var params = $state.params;
 
+    ctrl.today = $filter('date')(new Date(), AppConfig.DATE_FORMAT);
     ctrl.quotation = {};
     ctrl.quotation.customer = {};
     ctrl.quotation.goodsList = {};
@@ -15,14 +16,25 @@ angular.module('starter')
       if ($state.$current.name == "quotation.new") {
         ctrl.quotation.createDate = $filter('date')(new Date(), AppConfig.DATE_FORMAT);
 
-        if (params && params.obj && params.obj.stockNo) {
-          var goods = {};
-          goods.itemId = params.obj.stockNo;
-          goods.itemName = params.obj.stockName;
-          goods.unitPrice = params.obj.price;
-          goods.amount = 2;
+        if (params) {
+          if (params.flag == 'new') {
 
-          ctrl.quotation.goodsList[goods.itemId] = goods;
+          } else {
+            var quot = sessionStorage.getItem("quotation");
+            if (quot) {
+              ctrl.quotation = JSON.parse(quot);
+            }
+          }
+          if (params.obj && params.obj.stockNo) {
+            var goods = {};
+            goods.itemId = params.obj.stockNo;
+            goods.itemName = params.obj.stockName;
+            goods.unitPrice = params.obj.price;
+            goods.amount = 1;
+
+            ctrl.quotation.goodsList[goods.itemId] = goods;
+            sessionStorage.setItem("quotation", JSON.stringify(ctrl.quotation));
+          }
         }
       }
     }
@@ -30,10 +42,28 @@ angular.module('starter')
     ctrl.removeGoods = function(itemId) {
       if (ctrl.quotation.goodsList[itemId]) {
         delete ctrl.quotation.goodsList[itemId];
+        sessionStorage.setItem("quotation", JSON.stringify(ctrl.quotation));
       }
     }
+
+        ctrl.addAmount = function(itemId) {
+          if (ctrl.quotation.goodsList[itemId]) {
+            //ctrl.quotation.goodsList[itemId].amount++;
+            sessionStorage.setItem("quotation", JSON.stringify(ctrl.quotation));
+          }
+        }
 
     ctrl.init();
 
     $log.info("QuotationCtrl", "-- end --");
   })
+
+.filter('range', function() {
+  return function(input, min, max) {
+    min = parseInt(min); //Make string input int
+    max = parseInt(max);
+    for (var i = min; i <= max; i++)
+      input.push(i);
+    return input;
+  };
+});
