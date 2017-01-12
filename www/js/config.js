484d42c78ceed49c2ea0be0518fd00637e36898d
JerryHu <jerry_hu@wistron.com>
2017-01-12 09:53:56 +0800

    update version

--- www/js/config.js
+++ www/js/config.js
@@ -1,5 +1,5 @@
 var AppConfig = {
-  VERSION: 'v1.20 Build 2017.01.09.1',
+  VERSION: 'v1.21 Build 2017.01.12.1',
   DEBUG_MODE: false,
   API_URL: 'http://210.242.161.120:33880/tablet/api',
   //API_URL: 'http://web.mygesoft.com:33880/tablet/api',
